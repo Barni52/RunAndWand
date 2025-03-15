4dc0aa40691cc7c506b4e702865830760d4a541c
Barni52 <szabo.barnabas.mihaly@gmail.com>
2025-03-16 00:20:10 +0100

    added runnable exes

--- src/textureLoader.cpp
+++ src/textureLoader.cpp
@@ -12,10 +12,10 @@ TextureLoader::~TextureLoader() {
 }
 
 bool TextureLoader::loadTextures() {
-	playerTexture = LoadTexture("resources/player.png");
-	enemyTexture1 = LoadTexture("resources/enemy1.png");
-	projectileTexture1 = LoadTexture("resources/projectile1.png");
-	grassTexture = LoadTexture("resources/grass.png");
+	playerTexture = LoadTexture("../resources/player.png");
+	enemyTexture1 = LoadTexture("../resources/enemy1.png");
+	projectileTexture1 = LoadTexture("../resources/projectile1.png");
+	grassTexture = LoadTexture("../resources/grass.png");
 
 	Image icon = LoadImage("resources/RAWicon.png");
 	SetWindowIcon(icon);
